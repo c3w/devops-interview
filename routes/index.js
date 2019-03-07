148fd3d40bc9ba636ea0e9df96b6ea25e913c22a
Mark Chow <mark@agentiq.com>
2019-03-06 23:49:20 -0800

    add python execution

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const shell = require('shelljs');
 
 router.get('/ping', function(req, res, next) {
+  shell.exe('python3 --version');
   res.json({ health: 'healthy'});
 });
 
